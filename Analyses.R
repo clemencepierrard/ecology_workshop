716c73f23df4b7cac9a78019cb5baa09eeca9003
Clemence Pierrard <195500183+clemencepierrard@users.noreply.github.com>
2025-01-18 15:18:17 -0500

    Ajouter la couleur

--- Analyses.R
+++ Analyses.R
@@ -6,3 +6,6 @@ library(ratdat)
 ggplot(data=complete_old, aes(x=weight, y = hindfoot_lenght))+
   geom_point()
 
+#Ajout de la variable de la couleur
+ggplot(data=complete_old, aes(x=weight, y = hindfoot_lenght))+
+  geom_point(color="blue")
